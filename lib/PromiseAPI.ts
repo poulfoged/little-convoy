9a42b4a62a0b75da2f8e96f1edc847f073e3a1d6
Poul Foged Nielsen <poul@poulfoged.com>
2014-03-15 12:38:50 +0700

    Updated

---
+++ lib/PromiseAPI.ts
@@ -0,0 +1,12 @@
+﻿//interface IPromise {
+//    then(s?, e?, p?): IPromise;
+//    progressed(p): IPromise;
+//    catch(e): IPromise;
+//} 
+
+interface IPromise<T> {
+    then(success?: (data: T) => any, error?: (data: T) => any, progress?: (data: T) => any): IPromise<T>;
+    progressed(progressCallback: (data: any) => any): IPromise<T>;
+    catch(errorCallback: (data: T) => any): IPromise<T>;
+    finally(alwaysCallback: (data: T) => any): IPromise<T>;
+} 
